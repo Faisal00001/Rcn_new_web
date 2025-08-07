a2582bb488f3fa422ba9b98dd714c2b631c15e22
Faisal00001 <faisalosman798@gmail.com>
2025-08-07 15:09:20 +0600

    Changes php backend

--- backend/public/contact.php
+++ backend/public/contact.php
@@ -9,10 +9,12 @@ if ($_SERVER['REQUEST_METHOD'] === 'OPTIONS') {
     exit;
 }
 
+// CORS for actual requests
 header("Access-Control-Allow-Origin: *");
 header("Access-Control-Allow-Methods: POST, OPTIONS");
 header("Access-Control-Allow-Headers: Content-Type");
 header('Content-Type: application/json');
+
 
 // Composer autoloader
 require __DIR__ . '/../vendor/autoload.php';
